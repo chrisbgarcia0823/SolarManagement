57000120d8144e3072304974e2b24dd2542c7b66
Christian Garcia <chrisbgarcia0823@outlook.com>
2024-04-20 13:24:08 +0800

    Nav Icons Change

---
+++ Helpers/NavigationHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarManagement.Helpers
+{
+    public static class NavigationHelpers
+    {
+        public static string MakeActiveClass(this IUrlHelper urlHelper, string controller, string method)
+        {
+            try
+            {
+                string result = "active";
+                string controllerName = urlHelper.ActionContext.RouteData.Values["controller"].ToString();
+                string methodName = urlHelper.ActionContext.RouteData.Values["action"].ToString();
+                if (string.IsNullOrEmpty(controllerName)) return null;
+                if (controllerName.Equals(controller, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (methodName.Equals(method, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return result;
+                    }
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
