4eac425d0eaad1697fa41e73b100c48ad9b078e6
Christian Garcia <chrisbgarcia0823@outlook.com>
2024-06-11 20:35:13 +0800

    Table View and Method Added in Load Monitoring Controller

--- Controllers/LoadMonitoringController.cs
+++ Controllers/LoadMonitoringController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SolarManagement.Data;
 
 namespace SolarManagement.Controllers
 {
     public class LoadMonitoringController : Controller
     {
+        private readonly SolarManagementContext _context;
+
+        public LoadMonitoringController(SolarManagementContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Summary()
         {
             return View();
+        }
+
+        public async Task<IActionResult> TableView()
+        {
+            var query = from p in _context.powertbl select p;
+
+            var queryList = await query.ToListAsync();
+
+            return View(queryList);
         }
 
         public IActionResult CriticalLoad()
